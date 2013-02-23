0bbcb003bc2e0b137913a12b9992389b833ab0a1
demismg72 <demismg72@gmail.com>
2013-02-23 17:07:39 +0000

    tela do Administrador em PHP

---
+++ Projeto/Arquivos PHP e HTML/TelaDoAdministrador.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>
+		ProntOnline - Administrador Sistema
+	</title>
+</head>
+	<body>
+		<p align="center"><b><i>ProntOnLine</i></b>
+		<hr>
+		<br>
+		<br>
+		<hr>
+		<p align="center"><i>Cadastrar Novo Usuario</i></p>
+		<form action="Post" method="TelaDoAdministrador.php">
+			<p align="center">
+				<i>Informe o Tipo de Usuário</i>
+				<select>
+					<option>Medico</option>
+					<option>Enfermeira(o)</option>
+					<option>Paciente</option>
+				</select>
+			</p>
+		</form>
+	</body>
+</html>
+<?php
+?>
