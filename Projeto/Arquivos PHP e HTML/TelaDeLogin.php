8bb88e39522e0cafb26640dc65c5df3e3578868c
demismg72@gmail.com <demismg72@gmail.com@2fd2d0a1-2e32-7a1f-8c1d-5bcd9a12601d>
2013-02-23 17:06:22 +0000

    Tela de Login em php

---
+++ Projeto/Arquivos PHP e HTML/TelaDeLogin.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>
+		ProntOnLine - Login
+	</title>
+</head>
+	<body>
+		<p align="center"><i><b>ProntOnLine</b></i></p>
+		<hr>
+		<br>
+		<br>
+		<br>
+		<form action="Post" method="TelaDeLogin.php">
+			<p align="center"><b>Login: </b><input type="text" name="LoginTextBox"></p>
+			<p align="center"><b>Senha: </b><input type="text" name="SenhaTextBox"></p>
+            <p align="center"><input type="submit" name="EntrarBotao"></p>
+		</form>
+	</body>
+</html>
+<?php
+	 
+?>
