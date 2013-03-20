ad1cfd5817e2e0d31bb083e42beadf057fd441b1
guilapabsi <guilapabsi@gmail.com>
2013-03-20 16:50:14 +0000

    Iniciada da a tela do paciente.

---
+++ Projeto/Telas Dos Usuarios/TelaDoPaciente.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>
+		ProntOnLine - Paciente
+	</title>
+</head>
+<body>
+	<p align="center"><i><b>ProntOnLine</b></i></p>
+	<hr>
+	<br>
+	<br>
+	<br>
+	<p align="center">
+	<b>Nesta página você paciente pode marcar suas consultas para médicos de acordo com a especialidade </b>
+	<br>
+	<b>selecionada e visualizar seu prontuário.</b>
+	<br>
+	</p>
+	<br>
+	<p align="center"><a href="TelaDoPacienteMarcacao.php">Clique aqui para MARCAR CONSULTAS</a></p>
+	<p align="center"><a href="TelaDoPacienteProntuario.php">Clique aqui para VISUALIZAR SEU PRONTUARIO</a></p>
+	<br>
+</body>
+</html>
+<?php
+?>
