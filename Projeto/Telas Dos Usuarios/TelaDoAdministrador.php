f9ce388d6e180c5992466940139da01f9c7bffa4
guilapabsi@gmail.com <guilapabsi@gmail.com@2fd2d0a1-2e32-7a1f-8c1d-5bcd9a12601d>
2013-04-03 17:15:50 +0000

    Visão do administrador no sistema.

---
+++ Projeto/Telas Dos Usuarios/TelaDoAdministrador.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>
+		ProntOnLine - Administrador
+	</title>
+</head>
+<body>
+	<p align="center"><i><b>ProntOnLine</b></i></p>
+	<hr>
+	<br>
+	<br>
+	<br>
+	<p align="center">
+	<b>Nesta página você administrador pode incluir ou excluir medicos, enfermeiros e novos categorias de funcionarios, </b>
+	<br>
+	<b>podera tambem visualizar uma lista completa de medicos, enfermeiros e pacientes cadastrados no sistema, alem de gerar relatorios.</b>
+	<br>
+	</p>
+	<br>
+	<p align="center"><a href="TelaDeCadastroNovoMedico.php">Clique aqui para CADASTRAR UM NOVO MEDICO(A)</a></p>
+	<br>
+	<p align="center"><a href="TelaDeListagemDeMedicos.php">Clique aqui para VISUALIZAR TODOS OS MEDICOS(AS)</a></p>
+	<br>
+	<p align="center"><a href="TelaDeCadastroNovaEnfermeira.php">Clique aqui para CADASTRAR UM NOVO ENFERMEIRO(A)</a></p>
+	<br>
+	<p align="center"><a href="TelaDeListagemDeEnfermagem.php">Clique aqui para VISUALIZAR TODOS OS ENFERMEIROS(AS)</a></p>
+	<br>
+	<p align="center"><a href="">Clique aqui para CADASTRAR UMA NOVA CATEGORIA DE FUNCIONARIO</a></p>
+	<br>
+	<p align="center"><a href="TelaDeListagemDePacientes.php">Clique aqui para VISUALIZAR TODOS OS PACIENTES</a></p>
+	<hr>
+</body>
+</html>
+
+<?php
+
+?>
